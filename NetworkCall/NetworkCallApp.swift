d45025ebf521daf205f5d239542a7bcf3cd3cffb
Sunjay Kalsi <sunjay.kalsi@gmail.com>
2024-06-22 17:15:59 +0100

    Initial Commit

---
+++ NetworkCall/NetworkCallApp.swift
@@ -0,0 +1,17 @@
+//
+//  NetworkCallApp.swift
+//  NetworkCall
+//
+//  Created by Sunjay Kalsi on 22/06/2024.
+//
+
+import SwiftUI
+
+@main
+struct NetworkCallApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
