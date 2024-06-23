67d5d1d5589c979a566e4c3f648d02e15978b4d3
Sunjay Kalsi <sunjay.kalsi@gmail.com>
2024-06-23 12:30:52 +0100

    Add Async Network call

--- NetworkCall/ContentView.swift
+++ NetworkCall/ContentView.swift
@@ -8,17 +8,66 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var user: GitHubUser?
+
     var body: some View {
         VStack {
             Image(systemName: "globe")
                 .imageScale(.large)
                 .foregroundStyle(.tint)
-            Text("Hello, world!")
+            Text("Let's do a network call")
+            if let user {
+                VStack {
+                    Text(user.login)
+                    Text(user.bio)
+                    Image(user.avatarUrl)
+                }
+            }
         }
         .padding()
+        .task {
+            do {
+                user = try await getUser()
+            } catch {
+                print("Error \(error)")
+            }
+        }
+    }
+    
+    func getUser() async throws -> GitHubUser {
+        let endpoint = "https://api.github.com/users/sallen0400"
+        guard let url = URL(string: endpoint) else {
+            throw GHError.invalidUrl
+        }
+
+        let (data, response) = try await URLSession.shared.data(from: url) // see the Tuple is of data and response
+
+        guard let response = response as? HTTPURLResponse, response.statusCode == 200 else {
+            throw GHError.invalidResponse
+        }
+
+        do {
+            let decoder = JSONDecoder()
+            decoder.keyDecodingStrategy = .convertFromSnakeCase
+            return try decoder.decode(GitHubUser.self, from: data)
+        } catch {
+            throw GHError.invalidData // decoding error, mismatch
+        }
     }
 }
 
 #Preview {
     ContentView()
+}
+
+struct GitHubUser: Codable {
+    let login: String
+    let avatarUrl: String
+    let bio: String
+}
+
+enum GHError: Error {
+    case invalidUrl
+    case invalidResponse
+    case invalidData
 }
